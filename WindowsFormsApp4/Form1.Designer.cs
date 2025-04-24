5e08918b1cadea1d9a0c76a8608b4735fd28d4f8
hakim <hakim@WIN-O29CBNLA5V6>
2025-04-24 15:03:46 -0700

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.lastButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.findButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CustomerID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email Address";
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(238, 27);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerID.TabIndex = 4;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(238, 60);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerName.TabIndex = 5;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(238, 89);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtPhoneNumber.TabIndex = 6;
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(238, 122);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtEmailAddress.TabIndex = 7;
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(182, 178);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 23);
+            this.previousButton.TabIndex = 8;
+            this.previousButton.Text = "<";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(263, 178);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 9;
+            this.nextButton.Text = ">";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // firstButton
+            // 
+            this.firstButton.Location = new System.Drawing.Point(111, 177);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(75, 23);
+            this.firstButton.TabIndex = 10;
+            this.firstButton.Text = "<<";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // lastButton
+            // 
+            this.lastButton.Location = new System.Drawing.Point(344, 178);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new System.Drawing.Size(75, 23);
+            this.lastButton.TabIndex = 11;
+            this.lastButton.Text = ">>";
+            this.lastButton.UseVisualStyleBackColor = true;
+            this.lastButton.Click += new System.EventHandler(this.lastButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(117, 242);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 12;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(219, 241);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 13;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(334, 241);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(75, 23);
+            this.findButton.TabIndex = 14;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(448, 240);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 15;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(283, 284);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 16;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.firstButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.form1_load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button lastButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        private int posn = 0;
+        SqlConnection cn;
+        DataSet ds;
+        SqlDataAdapter da;
+        SqlCommandBuilder cb;
+        DataTable custTable;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void form1_load(object sender, EventArgs e)
+        {
+            MakeConnection();
+            GenerateData();
+
+            if (ds.Tables.Contains("CustomersCopy"))
+            {
+                custTable = ds.Tables["CustomersCopy"];
+                if (custTable.Rows.Count > 0)
+                {
+                    PopulateFields();
+                }
+                else
+                {
+                    MessageBox.Show("No customers found in the table.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Table 'CustomersCopy' not found in DataSet.");
+            }
+        }
+
+
+        private void MakeConnection()
+        {
+            string connectionString = "Server=172.20.10.3,1433;Database=Dafesty;User Id=sa;Password=reallyStrongPwd123;";
+            cn = new SqlConnection(connectionString);
+            try
+            {
+                cn.Open();
+                MessageBox.Show("Connection Opened Successfully!");
+                // Connection stays open here
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                cn.Close(); // Close only on error
+            }
+            // You can now use `cn` outside this block if needed
+
+        }
+        private void GenerateData() 
+        {
+            string sSQL = "SELECT CustomerID, CustomerName, PhoneNumber, EmailAddress from Customers";
+            da = new SqlDataAdapter(sSQL, cn);
+            cb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "CustomersCopy");
+        }
+
+        private void PopulateFields() 
+        {
+            txtCustomerID.Text = custTable.Rows[posn]["CustomerID"].ToString();
+            txtCustomerName.Text = custTable.Rows[posn]["CustomerName"].ToString();
+            txtPhoneNumber.Text = custTable.Rows[posn]["PhoneNumber"].ToString();
+            txtEmailAddress.Text = custTable.Rows[posn]["EmailAddress"].ToString();
+
+        }
+
+        
+
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            posn--;
+            if(posn< 0)
+            {
+                posn = 0;
+            }
+            PopulateFields();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            //MakeConnection();
+            posn++;
+            //if posn is greater than row count
+            int numberofRows = custTable.Rows.Count;
+            int rowIndex = numberofRows - 1;
+            if(posn >= rowIndex) 
+            {
+                posn = rowIndex;
+            }
+            PopulateFields();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            Console.Write(txtCustomerID.Text);
+        }
+
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            posn = 0;
+            PopulateFields();
+        }
+
+        private void lastButton_Click(object sender, EventArgs e)
+        {
+            posn = custTable.Rows.Count - 1;
+            PopulateFields();
+        }
+    }
+}
